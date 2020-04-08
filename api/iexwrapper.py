98697142356af676ef7aaf70a41c2f7e7aff2f0f
BastiX2 <swibbeke@googlemail.com>
2020-04-08 21:02:37 +0200

    Add multiple stocks support

--- api/iexwrapper.py
+++ api/iexwrapper.py
@@ -3,14 +3,24 @@ import requests
 
 # API Token
 TOKEN = "pk_179782678501460184c8f015057c382a"
-SYMBOL = "AAPL"
+SYMBOLS = {"AAPL", "ATVI", "BASFY", "FDX", "NFLX", "NVAX", "PCELF", "SAP", "PSMMF", "SYIEF"}
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0"}
 
 if __name__ == '__main__':
-    first_name = "Stafen"
-    response = requests.get(f"https://cloud.iexapis.com/stable/stock/{SYMBOL}/quote?token={TOKEN}", headers=HEADERS)
-    data = json.loads(response.text)
+    results = []
+    for symbol in SYMBOLS:
+        response = requests.get(f"https://cloud.iexapis.com/stable/stock/{symbol}/quote?token={TOKEN}", headers=HEADERS)
+        data = json.loads(response.text)
+        results.append(data)
 
-    for attr in data:
-        value = data[attr]
-        print(f"{attr} : {value}")
+    # Print to console
+    header = '{:>25} {:>15} {:>15}  {:>10}'.format("name", "Close", "Current", "Change")
+    print(header)
+    for stock in results:
+        current_value = stock["latestPrice"]
+        old_value = stock["previousClose"]
+        name = stock["companyName"]
+        change = stock["change"]
+        change_percent = stock["changePercent"]
+        output = '{:>25} {:>15} {:>15}  {:>10}'.format(name,  old_value, current_value, change)
+        print(output)
